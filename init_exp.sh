c7f893deaf9a87828ca688c1883d91ba99f441d8
Bo Tang <btang@cs.utsa.edu>
2014-04-19 19:26:25 -0500

    Updated init_exp.sh
    Cross-domain assignment enabled by "-c" argument.
    
    Change-Id: Ib115b00ac59a3f8041d56ac6aad919041e23df80

--- init_exp.sh
+++ init_exp.sh
@@ -2,27 +2,123 @@
 
 source switchenv admin
 
-COUNTER=0
+TOTALDOMAIN=10
+TOTALUSERINADOMAIN=10
+TOTALPROJINADOMAIN=10
 
-while :
+if [ $# -ne 1 ]
+then
+    echo "USAGE: $0 -[r|d|u|a]"
+    exit -1
+fi
+
+if [ $1 == "-r" ]
+then
+    RCOUNTER=0
+    # creating roles
+    while [ $RCOUNTER -lt 10 ]
+    do
+        openstack role create "r$RCOUNTER"
+        echo "r$RCOUNTER created."
+        RCOUNTER=`expr $RCOUNTER + 1`
+    done
+    echo "Successfully added $RCOUNTER roles."
+    exit 0
+fi
+
+DCOUNTER=0
+# outer while loop creating domains
+while [ $DCOUNTER -lt $TOTALDOMAIN ]
 do
-    USER="u"
 
-	if [ $COUNTER -lt 10 ]
-	then
-	    DOMAIN="d0$COUNTER"
-        #USER="u0$COUNTER"
-	elif [ $COUNTER -lt 100 ]
-	then
-	    DOMAIN="d$COUNTER"
-	else
-	    echo "Counter overflow!"
-	    exit -1
-	fi
-	echo "ADDED: $DOMAIN"
-	COUNTER=`expr $COUNTER + 1`
-	if [ $COUNTER -eq 100 ]
-	then
-	    echo "Successfully added 100 domains"
-	    exit 0
+    if [ $DCOUNTER -lt 10 ]
+    then
+        DOMAIN="0$DCOUNTER"
+    elif [ $DCOUNTER -lt 100 ]
+    then
+        DOMAIN="$DCOUNTER"
+    else
+        echo "DCounter overflow!"
+        exit -1
+    fi
+
+    if [ $1 == "-d" ]
+    then
+        openstack domain create "d$DOMAIN"
+        echo "d$DOMAIN created."
+    fi
+    DID="$(openstack domain show "d$DOMAIN" | grep id | cut -d"|" -f3 | sed "s/\ //")"
+    #echo $DID
+
+    NEXTDOMAIN=$(echo "($DCOUNTER+1) % $TOTALDOMAIN" | bc)
+    if [ $NEXTDOMAIN -lt 10 ]
+    then
+        NEXTDOMAIN="0$NEXTDOMAIN"
+    fi
+    #echo "$NEXTDOMAIN"
+    #break
+
+    # Add Domain Trust in MySQL DB
+
+    UPCOUNTER=0
+
+    # inner while loop creating users and projects
+    while [ $UPCOUNTER -lt $TOTALUSERINADOMAIN ]
+    do
+
+        
+        if [ $UPCOUNTER -lt 10 ]
+        then
+            USER="u$DOMAIN""0$UPCOUNTER"
+            PROJ="p$DOMAIN""0$UPCOUNTER"
+            # Cross-domain project
+            CPROJ="p$NEXTDOMAIN""0$UPCOUNTER"
+        elif [ $UPCOUNTER -lt 100 ]
+        then
+            USER="u$DOMAIN""$UPCOUNTER"
+            PROJ="p$DOMAIN""$UPCOUNTER"
+            # Cross-domain project
+            CPROJ="p$NEXTDOMAIN""$UPCOUNTER"
+        else
+            echo "UPCounter overflow!"
+            break
+        fi
+        
+        if [ $1 == "-u" ]
+        then
+            openstack user create --domain "d$DOMAIN" --password "admin" "$USER"
+            openstack user set --password "admin" "$USER"
+            openstack project create --domain "d$DOMAIN" "$PROJ"
+            echo "ADDED: $USER $PROJ in d$DOMAIN (id=$DID)"
+        fi
+
+        if [ $1 == "-a" ]
+        then
+            # Assign userx to rolex in projectx
+            openstack role add --user "$USER" --project "$PROJ" "r$UPCOUNTER"
+            echo "$USER is assigned to r$UPCOUNTER in $PROJ"
+        fi
+
+        if [ $1 == "-c" ]
+        then
+            # Assign userx to rolex in projectx of the next domain
+            openstack role add --user "$USER" --project "$CPROJ" "r$UPCOUNTER"
+            echo "$USER is assigned to r$UPCOUNTER in $CPROJ"
+        fi
+        
+        UPCOUNTER=`expr $UPCOUNTER + 1`
+
+    done
+
+    DCOUNTER=`expr $DCOUNTER + 1`
+
 done
+
+case $1 in
+    "-d") echo "Successfully added $DCOUNTER domains";;
+    "-u") echo "Successfully added users and projects in $DCOUNTER domains";;
+    "-a") echo "Successfully added roles in $DCOUNTER domains";;
+    "-c") echo "Successfully added cross-domain roles in $DCOUNTER domains";;
+esac
+    
+exit 0
