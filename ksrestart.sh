2dafbf020dd4cbc81a8f1df2a954238af440b259
Bo Tang <botang.usa@gmail.com>
2014-04-21 03:09:54 -0500

    Fixed bug in ksrestart.sh

--- ksrestart.sh
+++ ksrestart.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-kill $(cat "/opt/stack/status/stack/key.pid")
+pid=$(pgrep -f keystone-all)
+kill $pid
 
 /opt/stack/keystone/bin/keystone-all \
     --config-file /etc/keystone/keystone.conf \
     --log-config /etc/keystone/logging.conf -d --debug 2>&1  > /dev/null & 
 echo $! >/opt/stack/status/stack/key.pid
+cat /opt/stack/status/stack/key.pid
