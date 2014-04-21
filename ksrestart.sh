44cf23d100800ac64c736e2a60a3cff8b3b39c91
Bo Tang <botang.usa@gmail.com>
2014-04-21 18:17:45 -0500

    working ksrestart.sh

--- ksrestart.sh
+++ ksrestart.sh
@@ -3,9 +3,9 @@
 pid=$(pgrep -f keystone-all)
 sudo kill $pid
 
-/opt/stack/keystone/bin/keystone-all \
+nohup /opt/stack/keystone/bin/keystone-all \
     --config-file /etc/keystone/keystone.conf \
-    --log-config /etc/keystone/logging.conf -d --debug & 
+    --log-config /etc/keystone/logging.conf -d --debug 2>&1 > /dev/null & 
 echo $! >"/opt/stack/status/stack/key.pid"
 cat "/opt/stack/status/stack/key.pid"
 
