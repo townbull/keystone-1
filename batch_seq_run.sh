96ff34f90378a2cd6fcc4cbc5bfc2752f0f911d2
Bo Tang <botang.usa@gmail.com>
2014-04-21 21:45:59 -0500

    Added Sequential test

---
+++ batch_seq_run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+# Keystone servers with capability of 2 to the power of each number
+KS0="10.245.122.97"
+KS1="10.245.122.98"
+KS2="10.245.122.99"
+KS3="10.245.123.32"
+KS4="10.245.123.54"
+
+# Database server for experiment result storage
+HOST="10.245.122.14"
+DB="osacdt2"
+
+$ksx=$1
+KSX="KSX$ksx"
+KSX=${!$KSX}
+
+    ext=10
+    while [ $ext -ge 1 ]
+    do
+        # execute intra-domain experiment
+        /opt/stack/keystone/run_exp_seq.sh $ksx -i $ext
+	echo "Result of $ksx-I$ext-xx sent to $HOST:$DB." 
+        
+        # execute cross-domain experiment
+        /opt/stack/keystone/run_exp_seq.sh $ksx -c $ext
+	echo "Result of $ksx-C$ext-xx sent to $HOST:$DB." 
+        ext=`expr $ext - 1`
+    done
+
+echo "Batch teses ends at: $(date +%Y%m%d-%T)"
+exit 0
